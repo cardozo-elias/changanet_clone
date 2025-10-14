d7d82e2738c2b868ff0f1d7d5115d8ee81d4f932
Tomas <moralestomas991@gmail.com>
2025-10-14 17:35:21 -0300

    Limpieza

--- Sprint 2 (09SEPTIEMBRE – 20SEPTIEMBRE) Búsqueda y Comunicación/changanet/changanet-backend/src/routes/searchRoutes.js
+++
@@ -1,9 +0,0 @@
-// src/routes/searchRoutes.js
-const express = require('express');
-const { searchProfessionals } = require('../controllers/searchController');
-
-const router = express.Router();
-
-router.get('/', searchProfessionals);
-
-module.exports = router;
--- Sprint 6  (04NOVIEMBRE– 15NOVIEMBRE)  SOLUCIONADO INTEGRACION FRONT & BACK/changanet/changanet-frontend/tests/unit/App.test.js
+++
@@ -1,9 +0,0 @@
-// tests/unit/App.test.js
-import { render, screen } from '@testing-library/react';
-import App from '../../src/App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Changánet/i);
-  expect(linkElement).toBeInTheDocument();
-});
